afa85d60e594639689832246c22e3c79f4fbaf58
Radon Rosborough <radon.neon@gmail.com>
2022-03-01 18:54:25 -0800

    Un-disable error handling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,10 @@ Changelog](https://keepachangelog.com/en/1.0.0/).
 
 * Documentation
     * Fixed a few typography issues in man page
+* Bugfixes - functionality
+    * Fixed issue where error handling was disabled, so all errors
+      would be ignored and a zero exit status would be reported in
+      almost all circumstances
 * Bugfixes - packaging
     * Failing installation via Homebrew on macOS has been corrected
     * PKGBUILD now uses pluses instead of underscores in pkgver,
--- pass-ln.bash
+++ pass-ln.bash
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+# Note: set -e has no effect whatsoever, due to a bug^H^H^Hfun,
+# clearly documented feature of POSIX [1], combined with the fact that
+# Pass (seemingly by accident) invokes extensions in a subshell.
+#
+# Therefore, we have to litter '|| exit' after every command in the
+# script, unfortunately.
+#
+# [1]: https://unix.stackexchange.com/q/65532
+set -uo pipefail
 
-usage() {
+ln_usage() {
     echo 'Usage: pass ln target link-name'
-}
-
-die() {
-    echo >&2 "$@"
-    exit 1
 }
 
 VERSION=git
@@ -28,7 +31,7 @@ cmd_version() {
 
 cmd_ln() {
     if [[ "$#" -ne 2 ]]; then
-        usage >&2; exit 1
+        ln_usage >&2; exit 1
     fi
     check_sneaky_paths "$@"
     local target="$1"
@@ -48,26 +51,31 @@ cmd_ln() {
         link_name_is_dir=true
     fi
     if [[ -n "${link_name_is_dir}" ]]; then
-        link_name="${link_name%/}/$(basename -- "${target}")"
+        link_name="${link_name%/}/$(basename -- "${target}")" || exit
     fi
     local link_realname="${link_name}"
     if [[ -z "${target_is_dir}" ]]; then
         link_realname+=".gpg"
     fi
-    link_name_dir="$(dirname -- "${link_name}")"
-    target_relative_path="$(realpath -m --relative-to="${link_name_dir}" -- "${target_realname}")"
+    link_name_dir="$(dirname -- "${link_name}")" || exit
+    target_relative_path="$(realpath -m --relative-to="${link_name_dir}" -- "${target_realname}")" || exit
     set_git "${PREFIX}/${link_realname}"
-    mkdir -p "${PREFIX}/${link_name_dir}"
-    ln -s "${target_relative_path}" "${PREFIX}/${link_realname}"
+    mkdir -p "${PREFIX}/${link_name_dir}" || exit
+    ln -s "${target_relative_path}" "${PREFIX}/${link_realname}" || exit
     git_add_file "${PREFIX}/${link_realname}" "Alias ${target} to ${link_name}."
 }
 
 if [[ "$#" -eq 0 ]]; then
-    usage >&2; exit 1
+    ln_usage >&2; exit 1
 fi
 
 case "$1" in
-    help|--help|-h) usage              ;;
+    help|--help|-h) ln_usage           ;;
     version|--version|-v) cmd_version  ;;
     *) cmd_ln "$@"                     ;;
 esac
+
+# Pass will source us and then exit unconditionally with status zero,
+# ignoring whatever we did. Avoid that and force an exit after
+# completion.
+exit
