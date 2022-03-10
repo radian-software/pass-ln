9116110a0ba0d584e386ae37cd5503240dafb392
Radon Rosborough <radon.neon@gmail.com>
2022-03-09 19:59:12 -0800

    Publish CI image to GHCR

---
+++ ci/update_image.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+: "${GITHUB_TOKEN}"
+
+ts="$(date +%s)"
+
+if [[ "$#" -eq 0 || "$1" != "-n" ]]; then
+    docker build . -t pass-ln
+    ./docker/run-in-docker.bash pass-ln shellspec
+fi
+
+image="ghcr.io/raxod502/pass-ln-ci:${ts}"
+
+echo "${GITHUB_TOKEN}" | docker login ghcr.io -u raxod502 --password-stdin
+
+docker tag pass-ln "${image}"
+docker push "${image}"
+
+echo "${image}" > ./ci/current_image
