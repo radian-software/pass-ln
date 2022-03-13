54f1dfe2290134e29f0e290866cb9a49ed270d13
Radon Rosborough <radon.neon@gmail.com>
2022-03-12 17:03:53 -0800

    Make docker entrypoint script not require sudo

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -2,13 +2,19 @@
 
 set -euo pipefail
 
+runuser=()
+if ! groupadd sudo 2>/dev/null; then
+    runuser+=(runuser -u docker)
+fi
+
 groupadd -g "$(stat -c %g "$PWD")" -o -p '!' -r docker
 useradd -u "$(stat -c %u "$PWD")" -g "$(stat -c %g "$PWD")" -o -p '!' -m -N -l -s /usr/bin/bash -G sudo docker
 
+mkdir -p /etc/sudoers.d
 tee > /etc/sudoers.d/docker <<"EOF"
 %sudo	ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
 
 runuser -u docker -- touch /home/docker/.sudo_as_admin_successful
 
-exec runuser -u docker -- "$@"
+exec "${runuser[@]}" -- "$@"
